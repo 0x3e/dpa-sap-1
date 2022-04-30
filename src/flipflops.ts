e785ce5b1d9dd2d615bc8622494767cbee9a75c5
0x3e <0x3e.com@gmail.com>
2022-04-30 18:24:46 +1000

    d flipflop and divider

--- src/flipflops.ts
+++ src/flipflops.ts
@@ -1,11 +1,14 @@
 import * as h from './helpers';
 import * as buffer from './buffers';
 import * as gate from './gates';
+
 export interface ISRLatch {
   s: boolean;
   r: boolean;
+
   q: boolean;
   not_q: boolean;
+
   input ( s:boolean, r:boolean ): number;
   processing (): void;
   output (): number;
@@ -14,11 +17,33 @@ export interface ISRLatch {
 export interface IDLatch {
   e: boolean;
   d: boolean;
+
   q: boolean;
+
   not_q: boolean;
+
   input ( e:boolean, d:boolean ): number;
   processing (): void;
   output (): number;
+}
+
+export interface IDFlipFlop {
+  d: boolean;
+  c: boolean;
+
+  q: boolean;
+  not_q: boolean;
+
+  input ( d:boolean, c:boolean ): number;
+  processing (): void;
+  output (): number;
+}
+export interface IDDivider {
+  i: boolean;
+
+  input ( i:boolean ): boolean;
+  processing (): void;
+  output (): boolean;
 }
 
 export class SRLatch implements ISRLatch {
@@ -26,8 +51,10 @@ export class SRLatch implements ISRLatch {
   r: boolean;
   q: boolean;
   not_q: boolean;
+
   nor0: gate.NOR;
   nor1: gate.NOR;
+
   constructor() {
     this.s = h.random_bit();
     this.r = h.random_bit();
@@ -38,6 +65,7 @@ export class SRLatch implements ISRLatch {
     this.processing();
     this.output();
   }
+
   input(s: boolean, r: boolean) {
     h.throw_on_NaB(s, r);
     this.s = s;
@@ -45,6 +73,7 @@ export class SRLatch implements ISRLatch {
     this.processing();
     return this.output();
   }
+
   processing(){
     this.q = this.nor0.input(this.r, this.nor1.output());
     this.not_q = this.nor1.input(this.q, this.s);
@@ -52,6 +81,7 @@ export class SRLatch implements ISRLatch {
     this.q = this.nor0.output();
     this.not_q = this.nor1.output();
   }
+
   output(){
     const out = h.bits_to_num([this.not_q,this.q]);
     return out;
@@ -61,12 +91,15 @@ export class SRLatch implements ISRLatch {
 export class DLatch implements IDLatch {
   e: boolean;
   d: boolean;
+
   q: boolean;
   not_q: boolean;
+
   not: buffer.NOT;
   and0: gate.AND;
   and1: gate.AND;
   sr_latch: SRLatch;
+
   constructor() {
     this.e = h.random_bit();
     this.d = h.random_bit();
@@ -79,6 +112,7 @@ export class DLatch implements IDLatch {
     this.processing();
     this.output();
   }
+
   input(e: boolean, d: boolean) {
     h.throw_on_NaB(e, d);
     this.e = e;
@@ -87,16 +121,98 @@ export class DLatch implements IDLatch {
 
     return this.output();
   }
+
   processing(){
     const step1 = this.not.input(this.d);
     const step2 = this.and0.input(step1, this.e);
     const step3 = this.and1.input(this.e, this.d);
     const step4 = this.sr_latch.input(step3,step2);
+
     this.q = ((step4 >> 0) % 2) == 1;
     this.not_q = ((step4 >> 1) % 2 ) == 1;
   }
 
   output(){
     return this.sr_latch.output();
+  }
+}
+
+export class DFlipFlop implements IDFlipFlop {
+  d: boolean;
+  c: boolean;
+
+  q: boolean;
+  not_q: boolean;
+
+  and: gate.AND[];
+  not0: buffer.NOT;
+  not1: buffer.NOT;
+  sr_latch0: SRLatch;
+  sr_latch1: SRLatch;
+
+  constructor() {
+    this.d = h.random_bit();
+    this.c = h.random_bit();
+    this.q = h.random_bit();
+    this.not_q = h.random_bit();
+
+    this.and = new Array(4).fill(0).map(() => { return new gate.AND() });
+    this.not0 = new buffer.NOT();
+    this.not1 = new buffer.NOT();
+    this.sr_latch0 = new SRLatch();
+    this.sr_latch1 = new SRLatch();
+
+    this.processing();
+    this.output();
+  }
+
+  input(d:boolean, c:boolean){
+    h.throw_on_NaB(d, c);
+    this.d = d;
+    this.c = c;
+    this.processing();
+
+    return this.output();
+  }
+
+  processing(){
+    const step1 = this.not0.input(this.d);
+    const step2 = this.and[0].input(this.d, this.c);
+    const step3 = this.and[1].input(step1, this.c);
+    this.sr_latch0.input(step2,step3);
+    const step4 = this.not1.input(this.c)
+    const step5 = this.and[2].input(this.sr_latch0.q, step4);
+    const step6 = this.and[3].input(this.sr_latch0.not_q, step4);
+    this.sr_latch1.input(step5,step6);
+
+    this.q = this.sr_latch1.q
+    this.not_q = this.sr_latch1.not_q
+  }
+
+  output(){
+    const out = h.bits_to_num([this.not_q,this.q]);
+    return out;
+  }
+}
+export class DDivider implements IDDivider {
+  i: boolean;
+  d_flipflop: DFlipFlop;
+
+  constructor() {
+    this.i = h.random_bit();
+    this.d_flipflop = new DFlipFlop();
+  }
+  input(i:boolean){
+    h.throw_on_NaB(i);
+    this.i = i;
+    this.processing();
+
+    return this.output();
+  }
+  processing(){
+    this.d_flipflop.input(this.d_flipflop.not_q, this.i)
+  }
+  output (){
+    return this.d_flipflop.q
   }
 }
--- test/flipflops.js
+++ test/flipflops.js
@@ -81,3 +81,101 @@ E/C |  D  |   Q   | !Q    | Comment
          '.      | 0 | "! "|  NaB  | NaB |')
   t.end()
 })
+tap.test('DFlipFlop  | D | C | !Q | Q |', t => {
+  var d_flipflop = new flipflop.DFlipFlop()
+  const first_run = d_flipflop.output()
+  d_flipflop.input(false, false)
+  d_flipflop.input(false, true)
+  d_flipflop.input(false, false)
+  d_flipflop.input(false, true)
+  t.equal(d_flipflop.input(false, true), 0b10,
+  '1:         | 0 | 1 |  1 | 0 |')
+  t.equal(d_flipflop.input(false, false),0b10,
+  '2:         | 0 | 0 |  1 | 0 |')
+  t.equal(d_flipflop.input(false, true), 0b10,
+  '3:         | 0 | 1 |  1 | 0 |')
+  t.equal(d_flipflop.input(false, false),0b10,
+  '4:         | 0 | 0 |  1 | 0 |')
+  t.equal(d_flipflop.input(true, true),  0b10,
+  '5:         | 1 | 1 |  1 | 0 |')
+  t.equal(d_flipflop.input(true, false), 0b01,
+  '6:         | 1 | 0 |  0 | 1 |')
+  t.equal(d_flipflop.input(true, true),  0b01,
+  '7:         | 1 | 1 |  0 | 1 |')
+  t.equal(d_flipflop.input(true, false), 0b01,
+  '8:         | 1 | 0 |  0 | 1 |')
+  t.equal(d_flipflop.input(false, true), 0b01,
+  '9:         | 0 | 1 |  0 | 1 |')
+  t.equal(d_flipflop.input(false, false),0b10,
+   '10:       | 0 | 0 |  1 | 0 |')
+  t.equal(d_flipflop.input(false, true), 0b10,
+   '11:       | 0 | 1 |  1 | 0 |')
+  t.equal(d_flipflop.input(true, false), 0b10,
+   '12:       | 1 | 0 |  1 | 0 |')
+  t.equal(d_flipflop.input(false, true), 0b10,
+   '13:       | 0 | 1 |  1 | 0 |')
+  t.equal(d_flipflop.input(false, false),0b10,
+   '14:       | 0 | 0 |  1 | 0 |')
+  t.equal(d_flipflop.input(false, true), 0b10,
+   '15:       | 0 | 1 |  1 | 0 |')
+  t.equal(d_flipflop.input(true, false), 0b10,
+   '16:       | 1 | 0 |  1 | 0 |')
+  t.equal(d_flipflop.input(true, true),  0b10,
+   '17:       | 1 | 1 |  1 | 0 |')
+  t.equal(d_flipflop.input(true, false), 0b01,
+   '18:       | 1 | 0 |  0 | 1 |')
+  t.equal(d_flipflop.input(true, true),  0b01,
+   '19:       | 1 | 1 |  0 | 1 |')
+  t.equal(d_flipflop.input(true, false), 0b01,
+   '20:       | 1 | 0 |  0 | 1 |')
+  t.equal(d_flipflop.input(true, true),  0b01,
+   '21:       | 1 | 1 |  0 | 1 |')
+  t.equal(d_flipflop.input(true, false), 0b01,
+   '22:       | 1 | 0 |  0 | 1 |')
+  t.equal(d_flipflop.input(true, true),  0b01,
+   '23:       | 1 | 1 |  0 | 1 |')
+  t.equal(d_flipflop.input(true, false), 0b01,
+   '24:       | 1 | 0 |  0 | 1 |')
+  t.equal(d_flipflop.input(false, true), 0b01,
+   '25:       | 0 | 1 |  0 | 1 |')
+  t.equal(d_flipflop.input(false, false),0b10,
+   '26:       | 0 | 0 |  1 | 0 |')
+  t.equal(d_flipflop.input(true, true),  0b10,
+   '27:       | 1 | 1 |  1 | 0 |')
+  t.equal(d_flipflop.input(false, false),0b01,
+   '28:       | 0 | 0 |  0 | 1 |')
+  t.equal(d_flipflop.input(true, true),  0b01,
+   '29:       | 1 | 1 |  0 | 1 |')
+  t.equal(d_flipflop.input(true, false), 0b01,
+   '30:       | 1 | 0 |  0 | 1 |')
+  t.equal(d_flipflop.input(true, true),  0b01,
+   '31:       | 1 | 1 |  0 | 1 |')
+  t.equal(d_flipflop.input(true, false), 0b01,
+   '32:       | 1 | 0 |  0 | 1 |')
+  t.equal(d_flipflop.input(true, true),  0b01,
+   '33:       | 1 | 1 |  0 | 1 |')
+  t.end()
+})
+tap.test('DDivider  | i | o |', t => {
+  var d_divider = new flipflop.DDivider()
+  const first_run = d_divider.output()
+  d_divider.d_flipflop.input(true, false)
+  d_divider.d_flipflop.input(true, true)
+  t.equal(d_divider.input(false),  true,
+   '1:        | 0 | 1 |')
+  t.equal(d_divider.input(true),  true,
+   '2:        | 1 | 1 |')
+  t.equal(d_divider.input(false),  false,
+   '3:        | 0 | 0 |')
+  t.equal(d_divider.input(true),  false,
+   '4:        | 1 | 0 |')
+  t.equal(d_divider.input(false),  true,
+   '5:        | 0 | 1 |')
+  t.equal(d_divider.input(true),  true,
+   '6:        | 1 | 1 |')
+  t.equal(d_divider.input(false),  false,
+   '7:        | 0 | 0 |')
+  t.equal(d_divider.input(true),  false,
+   '8:        | 1 | 0 |')
+  t.end()
+})
